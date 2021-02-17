9fe16ff141237f0f626ac7a49ec2d336520bba9c
e-graine <72887022+e-graine@users.noreply.github.com>
2021-02-17 16:57:19 +0100

    Update doc.js

--- doc.js
+++ doc.js
@@ -5,7 +5,13 @@ doc = {
         text:"test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test"
     },
     doc2 : {
-        title:"Doc sans image",
+        title:"Ceci est un titre",
+        image: "img/image.jpg",
         text:"test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test"
+    },
+    doc3 : {
+        title:"Réduire ses déchets",
+        image: "img/image.jpg",
+        text:"Le meilleur déchet c’est celui que l’on ne produit pas. Au quotidien vous pouvez refuser les déchets à usage unique. Pour cela il faut prévoir des contenants réutilisables, qui prennent peu de place : beewrap, gourde, sac en tissus."
     }
 }
